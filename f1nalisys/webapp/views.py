338d6261db6850e2ae583af901b5d1b345608ec0
Dário Matos <dario.matos@ua.pt>
2020-12-29 23:56:42 +0000

    Página tracks com todos os circuitos

--- f1nalisys/webapp/views.py
+++ f1nalisys/webapp/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from s4api.graphdb_api import GraphDBApi
 from s4api.swagger import ApiClient
 import json
+
+
 # Create your views here.
 
 
@@ -21,6 +23,7 @@ def hello(request):
     db_info = open_db()
     print(db_info)
 
+    # "@prefix dbc: < http: // dbpedia.org / resource / Category: >."
     info = """
         select * where { 
 	        ?s ?p ?o .
@@ -55,8 +58,66 @@ def teams(request):
 
 
 def tracks(request):
-    return None
+    db_info = open_db()
+    print(db_info)
+
+    # "@prefix dbc: < http: // dbpedia.org / resource / Category: >."
+    info = """
+            PREFIX dbo:	<http://dbpedia.org/ontology/>
+            PREFIX dbp:	<http://dbpedia.org/property/>
+            PREFIX dct:	<http://purl.org/dc/terms/> 
+            PREFIX dbr:	<http://dbpedia.org/resource/> 
+            PREFIX dbc: <http://dbpedia.org/resource/Category:>
+            PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+            select DISTINCT ?s ?o ?l ?i
+            where {
+                ?s dct:subject dbc:Formula_One_circuits .
+                ?s rdfs:label ?o .
+                ?s dbp:location ?l .
+                ?s dbo:thumbnail ?i
+                filter (lang(?o) = "en")
+            }  
+
+        """
+
+    payload_query = {"query": info}
+    res = db_info[1].sparql_select(body=payload_query,
+                                   repo_name=db_info[0])
+    res = json.loads(res)
+    print(res)
+    pistas=dict()
+    nome=""
+    novaNome=""
+
+    for e in res['results']['bindings']:
+        nome=e['o']['value']
+        if nome not in pistas:
+            pistas[nome]=dict()
+            pistas[nome]["Location"]=e['l']['value']
+            pistas[nome]["Picture"]=e['i']['value']
+        else:
+            pistas[nome]["Location"]=pistas[nome]["Location"]+", "+e['l']['value']
+        # if pista is novaPista:
+        #     pass
+        # else:
+        #     pistas[pista]=[]
+
+        #list.append(e['s']['value'])
+
+    print(pistas)
+
+    tparams = {
+        'lista': pistas
+    }
+
+    return render(request, 'tracks.html', tparams)
 
 
 def season(request):
     return None
+
+
+
+
+
+
