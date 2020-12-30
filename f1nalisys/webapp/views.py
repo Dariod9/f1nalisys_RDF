e9337b451e1587255a10bac8441d7f96b259488a
Pedro <pedralmeida.22.99@gmail.com>
2020-12-30 00:58:24 +0000

    init teams

--- f1nalisys/webapp/views.py
+++ f1nalisys/webapp/views.py
@@ -54,7 +54,72 @@ def drivers(request):
 
 
 def teams(request):
-    return None
+    db_info = open_db()
+    print(db_info)
+
+    team_names = """
+            PREFIX dct: <http://purl.org/dc/terms/>
+            PREFIX dbc: <http://dbpedia.org/resource/Category:>
+            PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+            PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+            PREFIX dbp: <http://dbpedia.org/property/>
+            PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+            select distinct ?team ?team_name ?link ?races ?wins ?poles
+            where { 
+                ?team dct:subject dbc:Formula_One_constructors .
+                ?team rdfs:label ?team_name .
+                filter (lang(?team_name) = "en") .
+                optional{
+                    ?team foaf:homepage ?link .   
+                }
+                optional{
+                    ?team dbp:races ?races .
+                    filter(datatype(?races) = xsd:integer)   
+                }
+                optional{
+                    ?team dbp:wins ?wins .
+                    filter(datatype(?wins) = xsd:integer)   
+                }
+                optional{
+                    ?team dbp:wins ?poles .
+                    filter(datatype(?poles) = xsd:integer)   
+                }
+            }
+                order by desc (?races)    
+        """
+
+    payload_query = {"query": team_names}
+    res = db_info[1].sparql_select(body=payload_query,
+                                   repo_name=db_info[0])
+    res = json.loads(res)
+    # print(res)
+    teams_info = []
+    for e in res['results']['bindings']:
+        print("e: ", e)
+        dt = dict()
+        dt['nome'] = e['team_name']['value']
+
+        if 'link' in e.keys():
+            dt['link'] = e['link']['value']
+
+        if 'races' in e.keys():
+            dt['races'] = e['races']['value']
+
+        if 'wins' in e.keys():
+            dt['wins'] = e['wins']['value']
+
+        if 'poles' in e.keys():
+            dt['poles'] = e['poles']['value']
+
+        teams_info.append(dt)
+
+    print(teams_info)
+
+    tparams = {
+        'info': teams_info
+    }
+
+    return render(request, 'teams.html', tparams)
 
 
 def tracks(request):
